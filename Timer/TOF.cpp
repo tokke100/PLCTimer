7176afe1c76adfbda625915d2e36907ff1ad8db1
Tokke <tokke100@gmail.com>
2020-02-21 08:14:51 +0100

    update
    changed loop to update
    added init values
    changed acc code to the one from TON

--- Timer/TOF.cpp
+++ Timer/TOF.cpp
@@ -1,11 +1,17 @@
 #include "Arduino.h"
-#include "Timer.h"
+#include "PLCTimer.h"
 
 TOF::TOF(int p){
     pre = p;
+    dn = false;
+    en = false;
+    tt = false;
+    acc = 0;
+    previousMillis = 0;
+    currentMillis = 0;
 }
 
-void TOF::loop(){
+void TOF::update(){
     if(en && !dn){
         dn = true;
         tt = false;
@@ -21,7 +27,8 @@ void TOF::loop(){
         }else {
           currentMillis = millis();
         }
-        acc = currentMillis - previousMillis;
+        acc += currentMillis - previousMillis;
+        previousMillis = currentMillis;
         if(acc >= pre){
           dn = false;
           tt = false;
